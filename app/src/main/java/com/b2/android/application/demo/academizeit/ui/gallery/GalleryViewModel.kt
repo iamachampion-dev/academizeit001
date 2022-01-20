b77e5a9260537d772ee90872e403c11051b96ed4
iamachampion-dev <96933202+iamachampion-dev@users.noreply.github.com>
2022-01-21 01:34:13 +0530

    Added android project files via upload
    first commit
    created a new project with Android Studio
    dragged whole of project structure and committing to get a start.

---
+++ app/src/main/java/com/b2/android/application/demo/academizeit/ui/gallery/GalleryViewModel.kt
@@ -0,0 +1,13 @@
+package com.b2.android.application.demo.academizeit.ui.gallery
+
+import androidx.lifecycle.LiveData
+import androidx.lifecycle.MutableLiveData
+import androidx.lifecycle.ViewModel
+
+class GalleryViewModel : ViewModel() {
+
+    private val _text = MutableLiveData<String>().apply {
+        value = "This is gallery Fragment"
+    }
+    val text: LiveData<String> = _text
+}
---
+++ app/src/main/java/com/b2/android/application/demo/academizeit/ui/home/HomeViewModel.kt
@@ -0,0 +1,13 @@
+package com.b2.android.application.demo.academizeit.ui.home
+
+import androidx.lifecycle.LiveData
+import androidx.lifecycle.MutableLiveData
+import androidx.lifecycle.ViewModel
+
+class HomeViewModel : ViewModel() {
+
+    private val _text = MutableLiveData<String>().apply {
+        value = "This is home Fragment"
+    }
+    val text: LiveData<String> = _text
+}
